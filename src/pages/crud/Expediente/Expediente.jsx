9455f254b7f081b1b5655989e6173ef2f106fb18
Fabio Acosta <fabioacosta00001@gmail.com>
2025-03-28 09:37:00 +0000

    Tailwind icons updated

--- src/pages/crud/Expediente/Expediente.jsx
+++ src/pages/crud/Expediente/Expediente.jsx
@@ -42,7 +42,11 @@ const Expediente = () => {
                             </tr>
                         </tbody>
                     </table>
-                    <Link to="/expedientes/crear" className="bg-red-700 text-white rounded-full text-5xl">+</Link>
+                    <Link to="/expedientes/crear" className="bg-red-700 text-white rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                    </Link>
                 </div>
             </div>
         </>
