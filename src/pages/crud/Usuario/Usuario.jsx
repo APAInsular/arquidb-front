4003d5d345183a3851c62247f298ae537d0a872a
Fabio Acosta <fabioacosta00001@gmail.com>
2025-03-29 16:25:50 +0000

    Vista CRUD Usuario creada

---
+++ src/pages/crud/Usuario/Usuario.jsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+
+const Usuario = () => {
+    return (
+        <>
+            <div>
+                <Link to="/" className="text-5xl">←</Link>
+                <div className="flex justify-center gap-x-10 mt-10 mb-5">
+                    <button type="button" className="bg-red-700 text-white py-2 px-6 rounded-full">Persona</button>
+                    <button type="button" className="bg-red-700 text-white py-2 px-6 rounded-full">Fecha</button>
+                    <button type="button" className="bg-red-700 text-white py-2 px-6 rounded-full">Fase</button>
+                    <button type="button" className="bg-red-700 text-white py-2 px-6 rounded-full">Otros</button>
+                </div>
+                <div className="text-center">
+                    <table className="table-fixed w-full mb-5">
+                        <thead className="bg-gray-200">
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Email</th>
+                                <th>Rol</th>
+                                <th>Documentos firmados</th>
+                                <th colSpan={3}>Acciones</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Pedro</td>
+                                <td>pedrops@gmail.com</td>
+                                <td>Visador</td>
+                                <td>50</td>
+                                <td>
+                                    <Link to="/usuarios/1" className="bg-red-700 text-white py-1 px-6 rounded-lg">Ver</Link>
+                                </td>
+                                <td>
+                                    <button type="button" className="bg-red-700 text-white py-1 px-6 rounded-lg">Editar</button>
+                                </td>
+                                <td>
+                                    <button type="button" className="bg-red-700 text-white py-1 px-6 rounded-lg">Borrar</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <Link to="/usuarios/crear" className="text-center flex justify-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 bg-red-700 text-white rounded-full">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                        </svg>
+                    </Link>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Usuario;
