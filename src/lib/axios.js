c91694078770912bc14a16cdea2dcd4c08b3d7db
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-05-14 17:17:26 +0200

    withcredent

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -5,6 +5,7 @@ const getToken = () => localStorage.getItem("auth_token");
 
 const axios = Axios.create({
   baseURL: import.meta.env.VITE_APP_BACKEND_URL,
+  withCredentials: false,
   headers: {
     "Accept": "application/json",
     Authorization: `Bearer ${getToken()}`
