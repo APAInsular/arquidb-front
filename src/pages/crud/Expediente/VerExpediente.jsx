60f4a9772516491c4f08ba1f2b4a31b6038231ee
Fabio Acosta <fabioacosta00001@gmail.com>
2025-05-22 23:24:05 +0000

    Document delete in progress

--- src/pages/crud/Expediente/VerExpediente.jsx
+++ src/pages/crud/Expediente/VerExpediente.jsx
@@ -23,10 +23,8 @@ const VerExpediente = () => {
     const [collegiates, setCollegiates] = useState([]);
 
     const [modalPhase, setModalPhase] = useState(false);
-    const [page, setPage] = useState(1);
-    const [totalPages, setTotalPages] = useState([]);
-    const [deletes, setDeletes] = useState(false);
-    const [openId, setOpenId] = useState(null);
+    const [modalDelete, setModalDelete] = useState(false);
+    const [deleteId, setDeleteId] = useState(false);
 
     useEffect(() => {
         if (expedients) {
@@ -85,6 +83,11 @@ const VerExpediente = () => {
         }
     }, [modalPhase]);
 
+    const deleteActivate = (id) => {
+        setModalDelete(true);
+        setDeleteId(id);
+    }
+
     if (!expedient || !expedientPhases || !clients || !collegiates) return <WebLoader />
     console.log(expedient);
     console.log(clients);
@@ -97,8 +100,8 @@ const VerExpediente = () => {
                 {modalPhase && (
                     <PhaseEditor expedientPhases={expedientPhases} setModalPhase={setModalPhase} />
                 )}
-                {deletes && (
-                    <Delete DatoId={deletes} type={"Documento"} onClose={() => setDeletes(false)} url={"document"} />
+                {modalDelete && (
+                    <Delete DatoId={deleteId} onClose={() => setModalDelete(false)} type={"Documento"} url={"document"} />
                 )}
                 <div className="flex justify-between p-2 mb-5">
                     <h3 className="text-3xl">{expedient.title}</h3>
@@ -261,31 +264,21 @@ const VerExpediente = () => {
                             <div className="mt-6">
                                 {expedientDocuments.map(({ phase, documents }) => (
                                     <div key={phase.id} className="gap-6">
-                                        <p className="text-center text-2xl font-semibold mb-2">Fase {phase.phase}</p>
+                                        <p className="text-center text-3xl font-semibold mb-2">Fase {phase.phase}</p>
                                         <div className="gap-4">
                                             {documents.map(document => (
-                                                <div key={document.id} className="grid grid-cols-2 py-1.5 px-4 hover:bg-[#bb2b46]/60 hover:text-white even:bg-[#bb2b46]/8 mt-2 cursor-pointer transition-all shrink-0 overflow-x-scroll">
-                                                    <Link to={import.meta.env.VITE_APP_BACKEND_URL + "/storage/" + document.name}
-                                                        className="">
-                                                        <p>Documento {document.id}</p>
-                                                    </Link>
+                                                <div key={document.id} className="grid grid-cols-2 py-1.5 px-4 hover:bg-[#bb2b46]/60 hover:text-white even:bg-[#bb2b46]/8 mt-2 transition-all shrink-0 overflow-x-scroll">
+                                                    <p className="text-center text-2xl font-semibold">Documento {document.id}</p>
                                                     <div>
-                                                        <div className="grid grid-cols-3 gap-2">
-                                                            <Link to={`/expedientes/${expedient.id}/show`} className="flex justify-center items-center bg-sky-300 text-sky-600 hover:bg-sky-600 hover:text-orange-300 cursor-pointer font-medium py-1 text-sm rounded-full">
+                                                        <div className="grid grid-cols-2 gap-2">
+                                                            <Link to={import.meta.env.VITE_APP_BACKEND_URL + "/storage/" + document.name} className="flex justify-center items-center bg-sky-300 text-sky-600 hover:bg-sky-600 hover:text-orange-300 cursor-pointer font-medium py-1 text-sm rounded-full">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                                                     <path d="M12 15a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
                                                                     <path fillRule="evenodd" d="M1.323 11.447C2.811 6.976 7.028 3.75 12.001 3.75c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113-1.487 4.471-5.705 7.697-10.677 7.697-4.97 0-9.186-3.223-10.675-7.69a1.762 1.762 0 0 1 0-1.113ZM17.25 12a5.25 5.25 0 1 1-10.5 0 5.25 5.25 0 0 1 10.5 0Z" clipRule="evenodd" />
                                                                 </svg>
                                                             </Link>
 
-                                                            <Link to={`/expedientes/${expedient.id}/editar`} className="flex justify-center items-center bg-orange-300 text-orange-600 hover:bg-orange-600 hover:text-orange-300 cursor-pointer font-medium py-1 text-sm rounded-full">
-                                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-                                                                    <path d="M21.731 2.269a2.625 2.625 0 0 0-3.712 0l-1.157 1.157 3.712 3.712 1.157-1.157a2.625 2.625 0 0 0 0-3.712ZM19.513 8.199l-3.712-3.712-8.4 8.4a5.25 5.25 0 0 0-1.32 2.214l-.8 2.685a.75.75 0 0 0 .933.933l2.685-.8a5.25 5.25 0 0 0 2.214-1.32l8.4-8.4Z" />
-                                                                    <path d="M5.25 5.25a3 3 0 0 0-3 3v10.5a3 3 0 0 0 3 3h10.5a3 3 0 0 0 3-3V13.5a.75.75 0 0 0-1.5 0v5.25a1.5 1.5 0 0 1-1.5 1.5H5.25a1.5 1.5 0 0 1-1.5-1.5V8.25a1.5 1.5 0 0 1 1.5-1.5h5.25a.75.75 0 0 0 0-1.5H5.25Z" />
-                                                                </svg>
-                                                            </Link>
-
-                                                            <button type="button" onClick={() => deleteActivate(expedient.id)} className="flex justify-center items-center bg-red-300 text-red-600 hover:bg-red-600 hover:text-red-300 cursor-pointer font-medium py-1 text-sm rounded-full">
+                                                            <button type="button" onClick={() => deleteActivate(document.id)} className="flex justify-center items-center bg-red-300 text-red-600 hover:bg-red-600 hover:text-red-300 cursor-pointer font-medium py-1 text-sm rounded-full">
                                                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
                                                                     <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 0 1 3.878.512.75.75 0 1 1-.256 1.478l-.209-.035-1.005 13.07a3 3 0 0 1-2.991 2.77H8.084a3 3 0 0 1-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 0 1-.256-1.478A48.567 48.567 0 0 1 7.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 0 1 3.369 0c1.603.051 2.815 1.387 2.815 2.951Zm-6.136-1.452a51.196 51.196 0 0 1 3.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 0 0-6 0v-.113c0-.794.609-1.428 1.364-1.452Zm-.355 5.945a.75.75 0 1 0-1.5.058l.347 9a.75.75 0 1 0 1.499-.058l-.346-9Zm5.48.058a.75.75 0 1 0-1.498-.058l-.347 9a.75.75 0 0 0 1.5.058l.345-9Z" clipRule="evenodd" />
                                                                 </svg>
