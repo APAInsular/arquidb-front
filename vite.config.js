6a4652546add317d4538e27d7a491b5e549025ea
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-05-25 21:39:27 +0200

    changes vite

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // if (id.includes('react')) {
-            //   return 'react';
-            // }
             if (id.includes('date-fns')) {
               return 'date-fns';
             }
-            if (id.includes('react-route-dom')) {
-              return 'react-route-dom';
+            if (id.includes('react-router-dom')) {
+              return 'react-router-dom';
             }
             if (id.includes('axios')) {
               return 'axios';
