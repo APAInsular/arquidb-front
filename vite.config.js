0e6db198c63edf08ba03fec044cc8714120920cc
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-05-25 21:41:19 +0200

    changes vite mejorado y refactorizado

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('date-fns')) {
-              return 'date-fns';
-            }
-            if (id.includes('react-router-dom')) {
-              return 'react-router-dom';
-            }
-            if (id.includes('axios')) {
-              return 'axios';
-            }
-            return 'vendor';
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 1000,
   },
 })
