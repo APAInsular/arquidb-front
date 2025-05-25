763c18e5804cdaa3086db9e13cb5a257fc5c6fce
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-05-25 21:35:14 +0200

    changes vite

--- vite.config.js
+++ vite.config.js
@@ -10,15 +10,18 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            // if (id.includes('react')) {
-            //   return 'react';
-            // }
-            // if (id.includes('date-fns')) {
-            //   return 'date-fns';
-            // }
-            // if (id.includes('axios')) {
-            //   return 'axios';
-            // }
+            if (id.includes('react')) {
+              return 'react';
+            }
+            if (id.includes('date-fns')) {
+              return 'date-fns';
+            }
+            if (id.includes('react-route-dom')) {
+              return 'react-route-dom';
+            }
+            if (id.includes('axios')) {
+              return 'axios';
+            }
             return 'vendor';
           }
         },
