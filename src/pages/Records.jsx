f8677a4fb079f61245bd97a66b499bff57304ee2
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-07-14 09:46:26 +0100

    ajuste historial

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -52,7 +52,7 @@ const Records = () => {
                                             'Collegiates': 'Colegiado',
                                             'users': 'Usuario',
                                             'expedient': 'Expediente',
-                                            'centers': 'Centros'
+                                            'centers': 'Centro'
                                         };
                                         return (
                                             <tr key={datos.id} className="hover:bg-[#bb2b46]/60 hover:text-white even:bg-[#bb2b46]/8  mt-2 cursor-pointer transition-all shrink-0 overflow-x-scroll text-start text-nowrap">
