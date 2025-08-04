86c355f1680bc13e2e13f0995fe54a91a0fe42df
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-08-04 10:01:32 +0100

    cambio axios

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -8,8 +8,16 @@ const axios = Axios.create({
   withCredentials: true,
   headers: {
     "Accept": "application/json",
-    Authorization: `Bearer ${getToken()}`
   },
+});
+
+// Interceptor: añade el token actual en cada petición
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("auth_token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default axios;
