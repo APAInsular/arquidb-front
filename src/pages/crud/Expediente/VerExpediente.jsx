b97ea20c29c20845c3c028a9527f658b38a98ca7
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-08-07 10:15:11 +0100

    cambio boton enviar

--- src/pages/crud/Expediente/VerExpediente.jsx
+++ src/pages/crud/Expediente/VerExpediente.jsx
@@ -370,7 +370,7 @@ const VerExpediente = () => {
                                     </div>
                                     <h3 className="text-lg font-semibold text-gray-800">Documentos asociados</h3>
                                 </div>
-                                <div>
+                                {/* <div>
                                     {user?.roles.map(u => u.name == "visor") ? (
                                         <button
                                             type="button"
@@ -391,7 +391,7 @@ const VerExpediente = () => {
                                             <p>Visar</p>
                                         </button>
                                     )}
-                                </div>
+                                </div> */}
                             </div>
 
                             <div className="mt-6 mb-10">
