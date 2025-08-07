3714a61b7698baa23f34fa8acc700283c7e11bc3
LuisGordilloTadeo2003 <luisgordi@outlook.com>
2025-08-07 09:26:40 +0100

    cambio boton enviar

--- src/components/modals/crud/DocumentSelector.jsx
+++ src/components/modals/crud/DocumentSelector.jsx
@@ -29,24 +29,50 @@ const DocumentSelector = ({ phase, setModalDocument, expedientDocuments, setExpe
 
     return (
         <>
-            {showUpdateFile && <UpdateFile onClose={() => setShowUpdateFile(false)} phase={phase} phaseDocuments={phaseDocuments} setPhaseDocuments={setPhaseDocuments} />}
+            {showUpdateFile && (
+                <UpdateFile
+                    onClose={() => setShowUpdateFile(false)}
+                    phase={phase}
+                    phaseDocuments={phaseDocuments}
+                    setPhaseDocuments={setPhaseDocuments}
+                />
+            )}
             <Default className="text-center w-1/3">
-                <div className="bg-white text-blue-500 p-2">
-                    <div className="mb-10 text-black p-2">
+                <div className="bg-white text-blue-500 p-2 flex flex-col h-full">
+                    <div className="mb-4 text-black p-2">
                         <div className="flex justify-between border-b space-x-4">
                             <button type="button" onClick={() => setModalDocument(false)}>X</button>
                             <h3 className="text-3xl">{phase}</h3>
                         </div>
                     </div>
-                    <button type="button" onClick={() => setShowUpdateFile(true)} className="w-full bg-gray-300 rounded-lg mb-10 text-8xl">+</button>
-                    {phaseDocuments.map((document, index) => {
-                        return (
-                            <div key={document.id || index} className="bg-blue-400 text-white rounded-full p-2 my-5">
+
+                    <button
+                        type="button"
+                        onClick={() => setShowUpdateFile(true)}
+                        className="w-full bg-gray-300 rounded-lg mb-4 text-8xl"
+                    >
+                        +
+                    </button>
+
+                    {/* Contenedor con scroll */}
+                    <div className="flex-1 overflow-y-auto mb-4 px-2">
+                        {phaseDocuments.map((document, index) => (
+                            <div
+                                key={document.id || index}
+                                className="bg-blue-400 text-white rounded-full p-2 my-2"
+                            >
                                 <p>{document.name}</p>
                             </div>
-                        );
-                    })}
-                    <button type="button" onClick={() => handleClick()} className="bg-blue-600 text-white rounded-full py-2 px-6 w-full hover:bg-blue-700 focus:ring-2 focus:ring-blue-400">Enviar</button>
+                        ))}
+                    </div>
+
+                    <button
+                        type="button"
+                        onClick={() => handleClick()}
+                        className="bg-blue-600 text-white rounded-full py-2 px-6 w-full hover:bg-blue-700 focus:ring-2 focus:ring-blue-400"
+                    >
+                        Enviar
+                    </button>
                 </div>
             </Default>
         </>
